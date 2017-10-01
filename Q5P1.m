3bed7d0cec3cb2d7cde9faca7a2fdd37e1bde03d
BigMarbz <matthewwang2020@u.northwestern.edu>
2017-09-30 20:59:07 -0500

    Fsolve Finished

--- Q5P1.m
+++ Q5P1.m
@@ -1,12 +1,14 @@
+
 % Declare variable values
+
 A = 0.5;
 B = 0.5;
 e = 0.01;
 
 
-% Plot N(P)
+% Plotting N(P) and formatting the plot
 
-fplot(@(x) (-1+x/(A+B*x)-e*x*x),[0 20])
+fplot(@(x) (-1+x/(A+B*x)-e*x*x),[0 20],'LineWidth',2)
 hold();
 y=0;
 x2=0;
@@ -17,3 +19,10 @@ title('N(P) as a function of P');
 xlabel('P value');
 ylabel('N(P)');
 grid on;
+
+
+x1 = fsolve(@myfunc,0)
+x2 = fsolve(@myfunc,6)
+
+
+
---
+++ myfunc.m
@@ -0,0 +1,6 @@
+function F = myfunc(x)
+A = 0.5;
+B = 0.5;
+e = 0.01;
+F= (-1+x/(A+B*x)-e*x*x);
+end

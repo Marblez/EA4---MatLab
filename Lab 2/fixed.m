a6fbdb2e8a86bbf5c9f751783daddeeeb50d6f77
BigMarbz <matthewwang2020@u.northwestern.edu>
2017-10-07 17:31:54 -0500

    EA4 Lab 2

--- Lab 2/fixed.m
+++ Lab 2/fixed.m
@@ -1,11 +1,14 @@
 % Initialize Variables
+Iin = 0.5;
+yinit= -pi/2;
 h = 1;
 tfin = 10;
-hbig = 1;
-hsmall = 1.e-12;
-hcut = 2.0;
-hraise = 1.05;
-e= 0.001;
+
+% hbig = 1;
+% hsmall = 1.e-12;
+% hcut = 2.0;
+% hraise = 1.05;
+% e= 0.001;
 
 
 
@@ -17,5 +20,11 @@ global count;
 count = 0;
 
 % Calling function yPrime
-y1=yprime(h);
+% LOOP
+
+h2=yprime(h,Iin);
+h = h+h2;
+if h>tfin
+    h = tfin;
+end
 
--- Lab 2/yprime.m
+++ Lab 2/yprime.m
@@ -1,5 +1,5 @@
-function y = yprime(x)
-
-
+function y = yprime(theta, Iin)
+global count;
+y = (1-cos(theta))+Iin*(1+cos(theta));
 count = count +1;
 end
